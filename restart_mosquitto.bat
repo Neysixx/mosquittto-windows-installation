7d5a6a18568d17c63b2e1199642e891def2021b6
Kyllian Senrens <kylliansenrens3004@gmail.com>
2025-04-24 12:10:56 +0200

    feat(client): Install MQTT Explorer

--- restart_mosquitto.bat
+++ restart_mosquitto.bat
@@ -3,4 +3,9 @@ echo Restarting Mosquitto SERVICE...
 net stop mosquitto >nul
 net start mosquitto
 echo ✅ Mosquitto restarted successfully.
+if exist "C:\Program Files\MQTT Explorer\MQTT-Explorer.exe" (
+    start "" "C:\Program Files\MQTT Explorer\MQTT-Explorer.exe"
+) else (
+    echo ⚠️ MQTT Explorer does not exist.
+)
 pause
